b08bd1fc40b686248d8c09e4a26ac9e4df401248
habibullin <rxletter@gmail.com>
2017-05-23 19:14:16 +0300

    simple form and Slim added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170427162056) do
-
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
@@ -27,5 +26,4 @@ ActiveRecord::Schema.define(version: 20170427162056) do
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
-
 end
